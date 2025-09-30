789d830f8d71dc44b27326634dc63572867230f4
jesicamatul <evelinmtlj@gmail.com>
2025-09-30 09:02:05 -0400

    Stating variables

---
+++ src/main/groovy/RentalCarCalculator.java
@@ -0,0 +1,45 @@
+public class RentalCarCalculator {
+    public static void main(String[] args) {
+        // store variables first
+        double priceOfBasicCarRentalPerDay = 29.99;
+        double priceOfOptionTollTag = 3.95;
+        double priceOfOptionGps= 2.95;
+        double priceOfOptionsRoadAssistance = 3.95;
+        float percentSurchargeForUnderage = .30f;
+        int userAgeLimit = 25;
+
+        //what values do i need to get from the user?
+        System.out.print("When do you want to rent the car?: ? ");
+        String pickupDate;
+
+        System.out.print("How many days do you need it?:   ");
+        int numberOfDays;
+
+        System.out.print("Do you want a tool tag (Y?N): " );
+        boolean optionTollTag;
+
+        System.out.print("Do you want a GPS (Y?N): ");
+        boolean optionGps;
+        System.out.print("Do you want road assistance?: ");
+        boolean roadSideAssistance;
+
+        System.out.print("")
+        int age;
+
+
+
+                //what needs to be calculated
+
+        double basicCarRentalAmount;
+        double optionsAmount;
+        double underageSurcharge;
+        double totalCost;
+
+        //display the results.
+
+
+
+
+
+    }
+}
